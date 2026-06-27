ff4d783ac81f2c803ef3420a931a312f30b61d5d
mateuskolap <mateuskolap@gmail.com>
2026-06-27 17:39:23 -0300

    refactor: order form layout

--- resources/js/pages/Orders/OrderForm.vue
+++ resources/js/pages/Orders/OrderForm.vue
@@ -159,7 +159,7 @@ const removeItem = (productId: number) => {
 
         <div class="grid gap-6 md:grid-cols-12">
             <!-- Left Side: Basic Info -->
-            <div class="order-2 space-y-6 md:order-1 md:col-span-4 min-w-0">
+            <div class="space-y-6 md:col-span-4 min-w-0">
                 <div
                     class="rounded-xl border border-sidebar-border/70 bg-card p-5 dark:border-sidebar-border"
                 >
@@ -244,7 +244,7 @@ const removeItem = (productId: number) => {
             </div>
 
             <!-- Right Side: Items Selection -->
-            <div class="order-1 space-y-6 md:order-2 md:col-span-8 min-w-0">
+            <div class="space-y-6 md:col-span-8 min-w-0">
                 <div
                     class="rounded-xl border border-sidebar-border/70 bg-card p-5 dark:border-sidebar-border"
                 >
